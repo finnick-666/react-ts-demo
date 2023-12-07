286d749a90c8ea57801d7c8d0000907ee71b802b
finnick-666 <finnick9387@gmail.com>
2023-12-07 15:20:39 +0800

    prettier代码格式化设置

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react/react-in-jsx-scope */
+import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
   return (
